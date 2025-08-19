ed0c876dbe5b8b8ac15a033c22d9b2a27ccfd044
GitHub Action <action@github.com>
2025-08-19 04:26:08 +0000

    Generate checksums for release v0.2.3

---
+++ checksum-Elixir.Lingua.Nif.exs
@@ -0,0 +1,12 @@
+%{
+  "liblingua_nif-v0.2.3-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:43d6cfc566f55e34440fd3709f00700529e81e83cff9a4fee47b88d6e6f08dae",
+  "liblingua_nif-v0.2.3-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:22227ba48bf33c012a04de44c6723470824d302a8141037ce08effb14e9510e8",
+  "liblingua_nif-v0.2.3-nif-2.17-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:dcad35562b4f5bbce99803542b44b8509ae11f375ff62060dd39067991feb5e3",
+  "liblingua_nif-v0.2.3-nif-2.17-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:170c1c81877cc7bc4f9fc25fb6b1ad6aa9735c0a2445900172e346e9ea12b7e0",
+  "liblingua_nif-v0.2.3-nif-2.17-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:43504b2299ca49e8047c85ccaf343a9dc2da191a5ddcb1df3cfe09bb76d4e216",
+  "liblingua_nif-v0.2.3-nif-2.17-x86_64-apple-darwin.so.tar.gz" => "sha256:6127ef0321f5d29c1cc634ae8252f2c20e7e543421d5bce265909371a028851e",
+  "liblingua_nif-v0.2.3-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:742cdab49b3aa55dca69549d3550609819e6bc8f204424a9e65c2b94db3c75d6",
+  "liblingua_nif-v0.2.3-nif-2.17-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:8c3503db09c97b02670ad2ad7a7bc1c416538c73788248af1ae8da53f37cb010",
+  "lingua_nif-v0.2.3-nif-2.17-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:2e9c29dc5412f4a45168a99ce4aebe96bc6874bcb244b675439e1c01625f381f",
+  "lingua_nif-v0.2.3-nif-2.17-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:431ed9c23d5881ef3e1438c350f9072ccd8ef70868cd61227b8a64fb0ee73900",
+}
